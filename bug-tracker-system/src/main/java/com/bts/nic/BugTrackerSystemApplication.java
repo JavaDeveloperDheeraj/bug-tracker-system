40a5d04f90971092490a55b02ba23277e71085e8
JavaDeveloperDheeraj <kumardheerajnxt@gmail.com>
2024-09-11 19:50:16 +0530

    commit test STS.

--- bug-tracker-system/src/main/java/com/bts/nic/BugTrackerSystemApplication.java
+++ bug-tracker-system/src/main/java/com/bts/nic/BugTrackerSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BugTrackerSystemApplication {
-
+	// Hello This is my app
 	public static void main(String[] args) {
 		SpringApplication.run(BugTrackerSystemApplication.class, args);
 		System.err.println("hello world");
